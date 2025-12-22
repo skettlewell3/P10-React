0caa63b57f18265034cb9bc6decc9c0190279837
Sam K <113443285+skettlewell3@users.noreply.github.com>
2025-12-22 12:13:22 +0000

    UI: introduce fixture status

--- reactp10/src/components/Fixtures/FixturesCard.jsx
+++ reactp10/src/components/Fixtures/FixturesCard.jsx
@@ -4,26 +4,55 @@ import ScoringBreakdownContainer from "./ScoringBreakdownContainer";
 
 export default function FixturesCard( { day, ko, fixtures, mode, subjectType } ){
 
+    const cardStatus = fixtures[0]?.fixture_status;
+
+    const statusMap = {
+        upcoming: {label: 'Upcoming', color: 'amber'},
+        live: {label: 'Live', color: 'green'},
+        finished: {label: 'Finished', color: 'red'}
+    };
+
+    const statusMeta = statusMap[cardStatus]
+
+
+
+
+
     return (
         <div className="fixturesCard">
             <div className="fixturesCardHeader">
-                <div className="day">{day}</div>
-                <div className="ko">{ko}</div>
+                <div className="fcHeaderCenter">
+                    <div className="day">{day}</div>
+                    <div className="ko">{ko}</div>
+                </div>
+
+                {statusMeta && (
+                    <div className={`fixtureStatus ${statusMeta.color}`}>
+                        <span className="dot" />
+                        {statusMeta.label}
+                    </div>
+                )}
             </div>
-            {fixtures.map((fixture) => (
-               <FixtureFieldsetDB 
-                    key={fixture.fixture_id}
-                    fixture={fixture}
-                    mode={mode}
-                    toggledContent={
-                        mode === "form" 
-                        ? <PredictPreviews />
-                        : <ScoringBreakdownContainer 
-                            subjectType={subjectType}
-                        />
-                    }
-                />
-            ))}
+
+            {fixtures.map((fixture) => {
+                const isUpcoming = fixture.fixture_status === 'upcoming';
+
+                const toggledContent = 
+                    mode === 'form'
+                        ? <PredictPreviews /> 
+                        : isUpcoming 
+                            ? <PredictPreviews />
+                            : <ScoringBreakdownContainer subjectType={subjectType} />;
+
+                return (
+                    <FixtureFieldsetDB 
+                         key={fixture.fixture_id}
+                         fixture={fixture}
+                         mode={mode}
+                         toggledContent={toggledContent}
+                     />
+                )                
+            })}
         </div>
     )
 }
