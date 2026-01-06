aaa357b4fc2733c594457401af632b9971c7165e
Sam K <113443285+skettlewell3@users.noreply.github.com>
2026-01-06 17:30:29 +0000

    feat: previewPills

--- reactp10/src/components/Fixtures/PredictPreviewPill.jsx
+++ reactp10/src/components/Fixtures/PredictPreviewPill.jsx
@@ -1,32 +1,52 @@
-export default function PredictPreviewPill({ filteredPredictions }) {
+export default function PredictPreviewPill({ filteredPredictions, subject }) {
     return (
         <>
         {filteredPredictions.map((pred, index) => {
+            const normalized = subject === "user"
+                ? {
+                    id: pred.user_id,
+                    name: pred.user_name,
+                    homeGoals: pred.pred_home_goals,
+                    awayGoals: pred.pred_away_goals,
+                    isPerfect10: false,
+                    class: "userPreviewPill"
+                }
+                : {
+                    id: pred.club_id,
+                    name: pred.club_name,
+                    homeGoals: pred.club_home_goals,
+                    awayGoals: pred.club_away_goals,
+                    isPerfect10: pred.club_name === "Perfect10",
+                    class: "clubPreviewPill"
+                };
+
             // Determine colour
             let background;
             let text;
 
-            if (pred.club_name === "Perfect10") {
+            if (normalized.isPerfect10) {
                 background = "#FFF";
                 text = "hsla(145, 52%, 26%, 1.00)";
             } else {
-                // Generate shades for remaining clubs
-                const shade = 26 + index * 30;
+                // Generate shades for remaining pills
+                const shade = 26 + index * 15;
                 background = `hsl(145, 52%, ${shade}%)`;
                 text = "#FFF";
             }
 
+
+
             return (
                 <div
-                    className="clubPreview"
-                    key={pred.club_id}
+                    className={`pillPreview ${normalized.class}`}
+                    key={normalized.id}
                     style={{ backgroundColor: background, color: text }}
                 >
-                    <div className="previewName">{pred.club_name}</div>
+                    <div className="previewName">{normalized.name}</div>
                     <div className="previewPrediction">
-                        <span className="homePreview">{pred.club_home_goals}</span>
+                        <span className="homePreview">{normalized.homeGoals}</span>
                         <span className="previewV">v</span>
-                        <span className="awayPreview">{pred.club_away_goals}</span>
+                        <span className="awayPreview">{normalized.awayGoals}</span>
                     </div>
                 </div>
             );
--- reactp10/src/components/Fixtures/PredictPreviews.jsx
+++ reactp10/src/components/Fixtures/PredictPreviews.jsx
@@ -1,26 +1,40 @@
 import { usePredictionsClub } from "../../hooks/usePredictionsClub"
 import PredictPreviewPill from "./PredictPreviewPill";
 
-export default function PredictPreviews({ fixture_id }) {
-    const { clubPredictions, loading } = usePredictionsClub();
+export default function PredictPreviews({ fixture_id, fixture_status, userPredictions }) {
+    const { clubPredictions, clubLoading } = usePredictionsClub();
 
-    if (loading) return <p>Loading</p>;
+    if (clubLoading) return <p>Loading</p>;
 
-    const filteredPredictions = clubPredictions.filter(
+    const filteredClubPredictions = clubPredictions.filter(
         pred => pred.fixture_id === fixture_id
     );
 
-    if (!filteredPredictions.length) return null;
+    const filteredUserPredictions = userPredictions.filter(
+        pred => pred.fixture_id === fixture_id
+    );
+
+    const userPreviewsVisible = 
+        fixture_status !== "upcoming" && filteredUserPredictions.length > 0;
 
     return (
         <div className="predictPreviewsContainer">
-            <div className="clubPreviews">            
-                <PredictPreviewPill 
-                    filteredPredictions={filteredPredictions}
-                />            
+            <div className="clubPreviews">  
+                {filteredClubPredictions.length > 0 && (
+                    <PredictPreviewPill 
+                        filteredPredictions={filteredClubPredictions}
+                        subject={"club"}
+                    />        
+                )}          
             </div>
+            
             <div className="previewInsights">
-
+                {userPreviewsVisible && (
+                    <PredictPreviewPill
+                        filteredPredictions={filteredUserPredictions}
+                        subject={"user"}
+                    />
+                )}
             </div>
         </div>
     )
