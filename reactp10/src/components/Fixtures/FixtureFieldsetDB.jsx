d417ba359bc97362295b9b5f7ea40314e5bf01e2
Sam K <113443285+skettlewell3@users.noreply.github.com>
2026-02-25 17:14:00 +0000

    fixtureFieldsetDB update

--- reactp10/src/components/Fixtures/FixtureFieldsetDB.jsx
+++ reactp10/src/components/Fixtures/FixtureFieldsetDB.jsx
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from "react";
 import { usePredictionsUser } from "../../hooks/usePredictionsUser";
 import { classifyTeamName } from "../../utils/utils";
 import { useUser } from "../../hooks/useUser";
+import { usePremLeagueTables } from "../../hooks/usePremLeagueTables";
 
 export default function FixtureFieldsetDB({ fixture, mode, toggledContent, canToggle }) {
   const { fixture_id, home_team, home_short, away_team, away_short, home_goals, away_goals } = fixture;
   const { user } = useUser();
+  const { overallTable } = usePremLeagueTables();
 
   const { userPredictions, loading } = usePredictionsUser();
 
   const [homeValue, setHomeValue] = useState('');
   const [awayValue, setAwayValue] = useState('');
+  
 
   useEffect(() => {
     if (loading || !user) return;
@@ -18,8 +21,8 @@ export default function FixtureFieldsetDB({ fixture, mode, toggledContent, canTo
     const prediction = userPredictions.find(
       p => p.fixture_id === fixture_id && p.user_id === user.user_id);
 
-    setHomeValue(prediction?.pred_home_goals ?? '');
-    setAwayValue(prediction?.pred_away_goals ?? '');
+      setHomeValue(prediction?.pred_home_goals ?? '');
+      setAwayValue(prediction?.pred_away_goals ?? '');
   }, [fixture_id, userPredictions, loading, user]);
 
   const [ expanded, setExpanded ] = useState(false);
@@ -30,12 +33,49 @@ export default function FixtureFieldsetDB({ fixture, mode, toggledContent, canTo
     setExpanded(prev => !prev);
   }
 
+  const [homePos, setHomePos] = useState('');
+  const [awayPos, setAwayPos] = useState('');
+
+  useEffect(() => {
+    if (mode !== "form") return;
+
+    const homeData = overallTable.find(h => h.team_name === home_team);
+    const awayData = overallTable.find(a => a.team_name === away_team);
+
+    setHomePos(homeData?.pos ?? '');
+    setAwayPos(awayData?.pos ?? '');
+  }, [overallTable, home_team, away_team, mode]);
+
+  function toOrdinalSpan(n) {
+  const s = ["th", "st", "nd", "rd"];
+  const v = n % 100;
+  const suffix = s[(v - 20) % 10] || s[v] || s[0];
+
+  return (
+    <span>
+      {n}<sup>{suffix}</sup>
+    </span>
+  );
+}
+
+
   return (
     <div className="fixtureFieldsetWrapper">
       <fieldset className="match" onClick={handleToggle}>
-        <div className={`team home ${classifyTeamName(home_team)}`} title={home_team}>
-          {home_short}
-        </div>
+        {mode === "form" ? (
+          <div className={`team pos home ${classifyTeamName(home_team)}`} title={home_team}>
+            <div className={`teamPos ${classifyTeamName(home_team)}`}>
+              {homePos ? toOrdinalSpan(homePos) : ''}
+            </div>
+            <div className="posName">
+              {home_short}
+            </div>
+          </div>
+        ) : (
+          <div className={`team home ${classifyTeamName(home_team)}`} title={home_team}>
+            {home_short}
+          </div>
+        )}
 
         {mode === "form" ? (
           <div className="homePred">
@@ -75,9 +115,24 @@ export default function FixtureFieldsetDB({ fixture, mode, toggledContent, canTo
           <div className="awayScore">{away_goals}</div>
         )}
 
-        <div className={`team away ${classifyTeamName(away_team)}`} title={away_team}>
+        {mode === "form" ? (
+          <div className={`team pos away ${classifyTeamName(away_team)}`} title={away_team}>
+            <div className="posName">
+              {away_short}
+            </div>
+            <div className={`teamPos ${classifyTeamName(away_team)}`}>
+              {awayPos ? toOrdinalSpan(awayPos) : ''}
+            </div>
+          </div>
+        ) : (
+          <div className={`team away ${classifyTeamName(away_team)}`} title={away_team}>
+            {away_short}
+          </div>
+        )}
+
+        {/* <div className={`team away ${classifyTeamName(away_team)}`} title={away_team}>
           {away_short}
-        </div>
+        </div> */}
       </fieldset>
 
       {expanded && toggledContent && (
